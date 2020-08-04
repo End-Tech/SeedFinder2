7ecf5bbb346cf6fb2849748dffd742da37ae9898
nnnik <nnnik@web.de>
2020-08-04 20:26:51 +0200

    Added rudamentary Spawn Biome analasys features

--- src/ch/endte/seedfinder/BiomeComposition.java
+++ src/ch/endte/seedfinder/BiomeComposition.java
@@ -1,6 +1,14 @@
 package ch.endte.seedfinder;
 
+import java.util.Comparator;
+import java.util.HashMap;
+
+import kaptainwutax.biomeutils.Biome;
+
 public class BiomeComposition {
+	
+	private final HashMap<Biome, Integer> composition = new HashMap<>();
+	private int entryCounts = 0;
 	
 	public BiomeComposition() {}
 	
@@ -9,6 +17,24 @@ public class BiomeComposition {
 	}
 	
 	public void fromString() {
+		
+	}
+	
+	public void addBiome(Biome b) {
+		addBiome(b, 1);
+	}
+	
+	public void addBiome(Biome b, int bCount) {
+		composition.compute(b, (bio ,i) -> (i==null) ? bCount : i+bCount);
+		entryCounts+= bCount;
+	}
+	
+	private class EntrySortComparator implements Comparator<Biome> {
+
+		@Override
+		public int compare(Biome arg0, Biome arg1) {
+			return Integer.compare(composition.getOrDefault(arg0, 0), composition.getOrDefault(arg1, 0));
+		}
 		
 	}
 }
