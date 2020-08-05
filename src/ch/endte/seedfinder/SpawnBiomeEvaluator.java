59472896a87de4b9880b0de69b07f286ed832c4a
nnnik <nnnik@web.de>
2020-08-05 12:34:43 +0200

    Changed radius to match spawn perimeter size

--- src/ch/endte/seedfinder/SpawnBiomeEvaluator.java
+++ src/ch/endte/seedfinder/SpawnBiomeEvaluator.java
@@ -5,7 +5,7 @@ import ch.endte.seedfinder.EvaluationTask.Result;
 
 public class SpawnBiomeEvaluator {
 	
-	public static int SIZE = 1111;
+	public static int SIZE = 555;
 	public static int RIM_SIZE = 10;
 	public static int SIZE_SQ = SIZE*SIZE;
 	public static int RIM_OUTERSIZE_SQ = (SIZE+RIM_SIZE)*(SIZE+RIM_SIZE);
